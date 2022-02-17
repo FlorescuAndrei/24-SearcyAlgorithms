a8d36e553cbf7a494965d9459562df832d6bdaed
DESKTOP-U8IU9TC\Andrei <florescua@yahoo.com>
2022-02-17 14:58:00 +0200

    Initial commit

---
+++ src/com/andrei/LinearSearch.java
@@ -0,0 +1,24 @@
+package com.andrei;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int[]intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println(linearSearch(intArray, -15));
+        System.out.println(linearSearch(intArray, 35));
+        System.out.println(linearSearch(intArray, 215));
+
+    }
+
+    public static int linearSearch(int[] input, int value){
+        for(int i = 0; i < input.length; i++){
+            if(input[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
